beb2727ff1238bf7181497c958475a442fd158f2
中畑暖人 <peanuter@harumaki-Pro.local>
2025-11-19 19:39:21 +0900

    w06_task01 commit

--- W06/w06_task01_main.js
+++ W06/w06_task01_main.js
@@ -46,7 +46,7 @@ class ScatterPlot {
             .range( [0, self.inner_width] );
 
         self.yscale = d3.scaleLinear()
-            .range( [0, self.inner_height] );
+            .range( [self.inner_height, 0] );
 
         self.xaxis = d3.axisBottom( self.xscale )
             .ticks(6);
@@ -54,11 +54,11 @@ class ScatterPlot {
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`);
 
-        self.yaxis = d3.axisRight( self.yscale )
+        self.yaxis = d3.axisLeft( self.yscale )
             .ticks(6);
 
         self.yaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_width})`);
+            .attr('transform', `translate(0, 0)`);
     }
 
     update() {
