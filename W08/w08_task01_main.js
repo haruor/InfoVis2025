d7205b5b08e517ac9ac302fde8e3b23761e268b8
中畑暖人 <peanuter@harumaki-Pro.local>
2025-12-04 11:36:57 +0900

    W08 commit

--- W08/w08_task01_main.js
+++ W08/w08_task01_main.js
@@ -1,4 +1,75 @@
-var data = [
+class BarChart {
+    constructor(config, data) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 128,
+            margin: config.margin || {top: 10, right: 10, bottom: 20, left: 60}
+        };
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        const { width, height, margin } = this.config;
+
+        this.svg = d3.select(this.config.parent)
+            .attr('width', width)
+            .attr('height', height);
+
+        this.chart = this.svg.append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+        this.inner_width = width - margin.left - margin.right;
+        this.inner_height = height - margin.top - margin.bottom;
+
+        // Initialize axis scales
+        this.xscale = d3.scaleLinear()
+            .range([0, this.inner_width]);
+
+        this.yscale = d3.scaleBand()
+            .range([0, this.inner_height])
+            .paddingInner(0.1);
+
+        // Initialize axes
+        this.xaxis = d3.axisBottom(this.xscale)
+            .ticks(5)
+            .tickSizeOuter(0);
+
+        this.yaxis = d3.axisLeft(this.yscale)
+            .tickSizeOuter(0);
+
+        // Append axis groups
+        this.xaxis_group = this.chart.append('g')
+            .attr('transform', `translate(0, ${this.inner_height})`);
+
+        this.yaxis_group = this.chart.append('g');
+    }
+
+    update() {
+        // Update scales
+        this.xscale.domain([0, d3.max(this.data, d => d.value)]);
+        this.yscale.domain(this.data.map(d => d.label));
+
+        // Update axes
+        this.xaxis_group.call(this.xaxis);
+        this.yaxis_group.call(this.yaxis);
+    }
+
+    render() {
+        // Draw bars
+        this.chart.selectAll("rect")
+            .data(this.data)
+            .join("rect")
+            .attr("x", 0)
+            .attr("y", d => this.yscale(d.label))
+            .attr("width", d => this.xscale(d.value))
+            .attr("height", this.yscale.bandwidth());
+    }
+}
+
+// データと設定を渡してクラスを使用
+const data = [
     {label:'Apple', value:100},
     {label:'Banana', value:200},
     {label:'Cookie', value:50},
@@ -6,50 +77,13 @@ var data = [
     {label:'Egg', value:80}
 ];
 
-var width = 256;
-var height = 128;
-var margin = {top:10, right:10, bottom:20, left:60};
+const config = {
+    parent: '#drawing_region',
+    width: 256,
+    height: 128,
+    margin: {top: 10, right: 10, bottom: 20, left: 60}
+};
 
-var svg = d3.select('#drawing_region')
-    .attr('width', width)
-    .attr('height', height);
-
-var chart = svg.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-const inner_width = width - margin.left - margin.right;
-const inner_height = height - margin.top - margin.bottom;
-
-// Initialize axis scales
-const xscale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.value)])
-      .range([0, inner_width]);
-
-const yscale = d3.scaleBand()
-      .domain(data.map(d => d.label))
-      .range([0, inner_height])
-      .paddingInner(0.1);
-
-// Initialize axes
-const xaxis = d3.axisBottom( xscale )
-      .ticks(5)
-      .tickSizeOuter(0);
-
-const yaxis = d3.axisLeft( yscale )
-      .tickSizeOuter(0);
-
-// Draw the axis
-const xaxis_group = chart.append('g')
-      .attr('transform', `translate(0, ${inner_height})`)
-      .call( xaxis );
-
-const yaxis_group = chart.append('g')
-      .call( yaxis );
-
-// Draw bars
-chart.selectAll("rect").data(data).enter()
-    .append("rect")
-    .attr("x", 0)
-    .attr("y", d => yscale(d.label))
-    .attr("width", d => xscale(d.value))
-    .attr("height", yscale.bandwidth());
+const barChart = new BarChart(config, data);
+barChart.update();
+barChart.render();
