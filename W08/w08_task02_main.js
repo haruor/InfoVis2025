094043983a343cfd21ea59c8c74f87716bf8f694
中畑暖人 <peanuter@harumaki-Pro.local>
2025-12-04 12:05:10 +0900

    W08 commit

--- W08/w08_task02_main.js
+++ W08/w08_task02_main.js
@@ -1,23 +1,87 @@
-var data = [
-    {x:0, y:100},
-    {x:40, y:5},
-    {x:120, y:80},
-    {x:150, y:30},
-    {x:200, y:50}
-];
+class LineChart {
+    constructor(config, data) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 128,
+            margin: config.margin || {top: 10, right: 10, bottom: 10, left: 10}
+        };
+        this.data = data;
+        this.init();
+    }
 
-var width = 256;
-var height = 128;
+    init() {
+        const { width, height, margin } = this.config;
 
-var svg = d3.select('#drawing_region')
-    .attr('width', width)
-    .attr('height', height);
+        this.svg = d3.select(this.config.parent)
+            .attr('width', width)
+            .attr('height', height);
 
-const line = d3.line()
-      .x( d => d.x )
-      .y( d => d.y );
+        this.chart = this.svg.append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-svg.append('path')
-    .attr('d', line(data))
-    .attr('stroke', 'black')
-    .attr('fill', 'none');
+        this.inner_width = width - margin.left - margin.right;
+        this.inner_height = height - margin.top - margin.bottom;
+
+        // スケールの設定
+        this.xscale = d3.scaleLinear()
+            .range([0, this.inner_width]);
+
+        this.yscale = d3.scaleLinear()
+            .range([this.inner_height, 0]);
+
+        // 軸の設定
+        this.xaxis = d3.axisBottom(this.xscale).ticks(5);
+        this.yaxis = d3.axisLeft(this.yscale).ticks(5);
+
+        // 軸グループの追加
+        this.xaxis_group = this.chart.append('g')
+            .attr('transform', `translate(0, ${this.inner_height})`);
+
+        this.yaxis_group = this.chart.append('g');
+    }
+
+    update() {
+        // スケールのドメインをデータに基づいて設定
+        this.xscale.domain([0, d3.max(this.data, d => d.x)]);
+        this.yscale.domain([0, d3.max(this.data, d => d.y)]);
+    }
+
+    render() {
+        // 折れ線の生成
+        const line = d3.line()
+            .x(d => this.xscale(d.x))
+            .y(d => this.yscale(d.y));
+
+        // 折れ線を描画
+        this.chart.append('path')
+            .datum(this.data)
+            .attr('d', line)
+            .attr('stroke', 'black')
+            .attr('fill', 'none');
+
+        // 軸を描画
+        this.xaxis_group.call(this.xaxis);
+        this.yaxis_group.call(this.yaxis);
+    }
+}
+
+// 外部CSVファイルからデータを読み込む
+d3.csv("w08_task02.csv").then(data => {
+    // データを数値に変換
+    data.forEach(d => {
+        d.x = +d.x;
+        d.y = +d.y;
+    });
+
+    const config = {
+        parent: '#drawing_region',
+        width: 256,
+        height: 128,
+        margin: {top: 10, right: 10, bottom: 20, left: 30}
+    };
+
+    const lineChart = new LineChart(config, data);
+    lineChart.update();
+    lineChart.render();
+});
